83cdb21e19e6333f91ad60aeb97c355758b1e6a6
Miguel García <mikelrapsolo@gmail.com>
2022-01-19 14:08:22 +0100

    Arquivo final

--- index.php
+++ index.php
@@ -7,6 +7,36 @@
     <title>Document</title>
 </head>
 <body>
+    <h3>Definicion de arrays en PHP</h3>
+    <?php
+    //Array indexado
+    $amigos=array("Anton","Rosalia","Xoan");
+    echo "O terceiro amigo é: " .$amigos[2]."<br/>";
+    //Array asociativo
+    $dnisAmigos=array("Anton"=>"12345678x","Rosalia"=>"11112222X","Xoan"=>"99997777X");
+    echo "O DNI de Rosalía é: " .$dnisAmigos['Rosalia']."<br/>";
+    //Array de duas dimensions:
+    $cochesAmigos = array
+    (
+        "Anton"=>array
+    (
+    "Fiat 500",
+    "Audi A3",
+    "Peugeot 208"
+    ),
+    "Rosalia"=>array
+    (
+        "Audi TT"
+    ),
+    "Xoan"=>array
+    (
+        "jaguar XF",
+        "BMW 520d",
+        "Seat Ibiza"
+    )
+    );
+    echo "O segundo coche de Xoan é " . $cochesAmigos['Xoan'][1] . "<br/>";
+    ?>
     
 </body>
 </html>
